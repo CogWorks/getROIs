ae787a6b923e4d12b512dce6036000b6abbbe186
Sounak Banerjee <sounakban.science@gmail.com>
2020-04-26 21:11:45 -0400

    Added variables to output class diagram, created first version of input class diagram, created first python script file

---
+++ source/Input_Classes.py
@@ -0,0 +1,20 @@
+class GameLog:
+    def __init__(gameDF):
+        playerID
+        sessionID
+        timeStamp
+        boardRep
+        zoidRep
+
+    pass
+
+class GazeLog:
+    def __init__(gazeDF):
+        playerID
+        sessionID
+        timeStamp
+        gazeX
+        gazeY
+        gazeZ
+
+    pass

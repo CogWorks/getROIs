2d880f3656e3e4c6b3e8c45f19442b47f2d1236b
SteveChris <13438833+SteveChris@users.noreply.github.com>
2020-05-05 00:26:50 -0400

    Clarified definitions
    Added comments and made a default constructor for GameLog so that I can test my functions. Actual read functions incoming someday

--- source/Input_Classes.py
+++ source/Input_Classes.py
@@ -1,20 +1,27 @@
 class GameLog:
-    def __init__(gameDF):
-        playerID
-        sessionID
-        timeStamp
-        boardRep
-        zoidRep
+    def __init__(self):
+        self.subjectID = 0  #e.g. 19CTWC007
+        self.sessionID = 0   #Identifier for which game in the study this is
+        self.timeStamp = []  #list of timestamps
+        self.boardRep  = []  #time series of board representations
+        self.zoidRep   = []  #time series of falling zoid representations
 
-    pass
 
 class GazeLog:
     def __init__(gazeDF):
-        playerID
-        sessionID
-        timeStamp
-        gazeX
-        gazeY
-        gazeZ
+        subjectID       #e.g. 19CTWC007
+        sessionID       #Identifier for which gaze recording in the study this is (usually unneeded for popstudy)
+        timeStamp       #list of timestamps
+        gazeX           #time series of X gaze coords
+        gazeY           #time series of Y gaze coords
+        gazeZ           #time series of Z gaze coords
 
     pass
+
+class SyncLog:
+    def __init__(syncDF):
+        system_clock    #time series of system (local) clock ticks
+        spectrum_clock  #time series of eyetracker (remote) clock ticks
+        
+
+ 

19e99510fe8ced09b4207ed57cda7636796adcac
Banerjee <baners8@cwl-projects.cogsci.rpi.edu>
2020-06-06 01:18:53 -0400

    restructuring

--- source/Parsers/GameParser_Meta2_SQL.py
+++ source/Parsers/GameParser_Meta2_SQL.py
@@ -1,16 +1,20 @@
+# Parameters
+gameList_Query = "SELECT gameID FROM GameSummaries WHERE filePath LIKE '%CTWC19%' AND ECID <> '9test' OR ECID <> 'ultraspeedtetris'"
+
+
 # Libraries
 from sqlalchemy import create_engine
 import pymysql
 import pandas as pd
 
-# Create connection with database
-sqlEngine = create_engine('mysql+pymysql://tetris_ro:CogWorksTetris@cwlsql.hass.rpi.edu/TetrisMetaTWO', pool_recycle=3600)
-dbConnection = sqlEngine.connect()
 
-query = "SELECT GameSummaries.gameID, SID, USID, SessionNum, GameNum, Window_Height, Window_Width, ts, system_ticks, board_rep, zoid_rep\
-        FROM GameSummaries INNER JOIN GameLogs ON GameSummaries.gameID = GameLogs.gameID\
-        WHERE GameSummaries.gameID IN (SELECT gameID FROM GameSummaries WHERE filePath LIKE '%CTWC19%' AND ECID <> '9test')"
-resultDF = pd.read_sql(query, dbConnection)
+
+
+gameList_df = pd.read_sql(gameList_Query, dbConnection)
+gameIDs = gameList_df['gameID']
+
+for id in gameIDs:
+        pass
 
 # query = "SELECT * FROM GameSummaries WHERE gameID = 5"
 # df = pd.read_sql(query, dbConnection)
@@ -19,3 +23,13 @@ resultDF = pd.read_sql(query, dbConnection)
 # df = pd.read_sql(query, dbConnection, params=(10))
 
 
+def parse(gameID):
+
+        # Create connection with database
+        sqlEngine = create_engine('mysql+pymysql://tetris_ro:CogWorksTetris@cwlsql.hass.rpi.edu/TetrisMetaTWO', pool_recycle=3600)
+        dbConnection = sqlEngine.connect()
+
+        query = "SELECT GameSummaries.gameID, SID, USID, SessionNum, GameNum, Window_Height, Window_Width, ts, system_ticks, board_rep, zoid_rep\
+                FROM GameSummaries INNER JOIN GameLogs ON GameSummaries.gameID = GameLogs.gameID\
+                WHERE GameSummaries.gameID IN"
+        resultDF = pd.read_sql(query, dbConnection)
---
+++ source/ROI_Codes/GenerateROI.py
@@ -0,0 +1,34 @@
+# Parameters
+gameList_Query = "SELECT gameID FROM GameSummaries WHERE filePath LIKE '%CTWC19%' AND ECID <> '9test' OR ECID <> 'ultraspeedtetris'"
+
+
+
+
+# Libraries
+from sqlalchemy import create_engine
+import pymysql
+import pandas as pd
+
+# Create connection with database
+sqlEngine = create_engine('mysql+pymysql://tetris_ro:CogWorksTetris@cwlsql.hass.rpi.edu/TetrisMetaTWO', pool_recycle=3600)
+dbConnection = sqlEngine.connect()
+
+
+gameList_df = pd.read_sql(gameList_Query, dbConnection)
+gameIDs = gameList_df['gameID']
+
+for id in gameIDs:
+        pass
+
+query = "SELECT GameSummaries.gameID, SID, USID, SessionNum, GameNum, Window_Height, Window_Width, ts, system_ticks, board_rep, zoid_rep\
+        FROM GameSummaries INNER JOIN GameLogs ON GameSummaries.gameID = GameLogs.gameID\
+        WHERE GameSummaries.gameID IN (SELECT gameID FROM GameSummaries WHERE filePath LIKE '%CTWC19%' AND ECID <> '9test' OR ECID <> 'ultraspeedtetris')"
+resultDF = pd.read_sql(query, dbConnection)
+
+# query = "SELECT * FROM GameSummaries WHERE gameID = 5"
+# df = pd.read_sql(query, dbConnection)
+
+# query = "SELECT * FROM GameLogs WHERE gameID = ?"
+# df = pd.read_sql(query, dbConnection, params=(10))
+
+
